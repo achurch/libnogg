e8ae13e3133de46d4ca7361535202a330f40402d
Andrew Church <achurch@users.noreply.github.com>
2014-05-25 13:43:52 +0000

    Remove a few unneeded environment-specific hacks in stb_vorbis.

--- src/stb_vorbis.c
+++ src/stb_vorbis.c
@@ -434,25 +434,10 @@ enum STBVorbisError
 
 //////////////////////////////////////////////////////////////////////////////
 
-#ifndef STB_VORBIS_NO_CRT
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <math.h>
-#if !(defined(__APPLE__) || defined(MACOSX) || defined(macintosh) || defined(Macintosh))
-#include <malloc.h>
-#endif
-#else
-#define NULL 0
-#endif
-
-#ifndef _MSC_VER
-   #if __GNUC__
-      #define __forceinline inline
-   #else
-      #define __forceinline
-   #endif
-#endif
 
 #if STB_VORBIS_MAX_CHANNELS > 256
 #error "Value of STB_VORBIS_MAX_CHANNELS outside of allowed range"
@@ -829,7 +814,7 @@ static void crc32_init(void)
    }
 }
 
-static __forceinline uint32 crc32_update(uint32 crc, uint8 byte)
+static inline uint32 crc32_update(uint32 crc, uint8 byte)
 {
    return (crc << 8) ^ crc_table[byte ^ (crc >> 24)];
 }
@@ -1424,7 +1409,7 @@ static int32 get_bits_signed(vorb *f, int n)
 // expand the buffer to as many bits as possible without reading off end of packet
 // it might be nice to allow f->valid_bits and f->acc to be stored in registers,
 // e.g. cache them locally and decode locally
-static __forceinline void prep_huffman(vorb *f)
+static inline void prep_huffman(vorb *f)
 {
    if (f->valid_bits <= 24) {
       if (f->valid_bits == 0) f->acc = 0;
@@ -1898,7 +1883,7 @@ static float inverse_db_table[256] =
 int8 integer_divide_table[DIVTAB_NUMER][DIVTAB_DENOM]; // 2KB
 #endif
 
-static __forceinline void draw_line(float *output, int x0, int y0, int x1, int y1, int n)
+static inline void draw_line(float *output, int x0, int y0, int x1, int y1, int n)
 {
    int dy = y1 - y0;
    int adx = x1 - x0;
@@ -2310,7 +2295,7 @@ void inverse_mdct(float *buffer, int n, vorb *f, int blocktype)
 
 
 // the following were split out into separate functions while optimizing;
-// they could be pushed back up but eh. __forceinline showed no change;
+// they could be pushed back up but eh. inline showed no change;
 // they're probably already being inlined.
 static void imdct_step3_iter0_loop(int n, float *e, int i_off, int k_off, float *A)
 {
@@ -2458,7 +2443,7 @@ static void imdct_step3_inner_s_loop(int n, float *e, int i_off, int k_off, floa
    }
 }
 
-static __forceinline void iter_54(float *z)
+static inline void iter_54(float *z)
 {
    float k00,k11,k22,k33;
    float y0,y1,y2,y3;
