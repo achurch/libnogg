738f58f4082763958bce4fa78712b0a36cc9ab99
Andrew Church <achurch@users.noreply.github.com>
2014-06-01 21:17:04 +0000

    Clean up the inline functions.

--- src/decode/inlines.h
+++ src/decode/inlines.h
@@ -20,8 +20,10 @@
 /*************************************************************************/
 /*************************************************************************/
 
-// used in setup, and for huffman that doesn't go fast path
-static unsigned int bit_reverse(unsigned int n)
+/**
+ * bit_reverse:  Reverse the order of the bits in a 32-bit integer.
+ */
+static inline UNUSED CONST_FUNCTION uint32_t bit_reverse(uint32_t n)
 {
   n = ((n & 0xAAAAAAAA) >>  1) | ((n & 0x55555555) << 1);
   n = ((n & 0xCCCCCCCC) >>  2) | ((n & 0x33333333) << 2);
@@ -30,21 +32,31 @@ static unsigned int bit_reverse(unsigned int n)
   return (n >> 16) | (n << 16);
 }
 
-static float square(float x)
+/*-----------------------------------------------------------------------*/
+
+/**
+ * square:  Return the square of the floating-point argument.
+ */
+static inline UNUSED CONST_FUNCTION float square(float x)
 {
    return x*x;
 }
 
-// this is a weird definition of log2() for which log2(1) = 1, log2(2) = 2, log2(4) = 3
-// as required by the specification. fast(?) implementation from stb.h
-// @OPTIMIZE: called multiple times per-packet with "constants"; move to setup
-static int ilog(int32_t n)
+/*-----------------------------------------------------------------------*/
+
+/**
+ * ilog2:  Return the Vorbis-style base-2 log of the argument, with any
+ * fractional part of the result truncated.  The Vorbis specification
+ * defines log2(1) = 1, log2(2) = 2, log2(4) = 3, etc.
+ */
+// FIXME: called multiple times per-packet with "constants"; move to setup
+static int ilog(uint32_t n)
 {
    static signed char log2_4[16] = { 0,1,2,2,3,3,3,3,4,4,4,4,4,4,4,4 };
 
    // 2 compares if n < 16, 3 compares otherwise (4 if signed or n > 1<<29)
    if (n < (1 << 14))
-        if (n < (1 <<  4))        return     0 + log2_4[n      ];
+        if (n < (1 <<  4))           return  0 + log2_4[n      ];
         else if (n < (1 <<  9))      return  5 + log2_4[n >>  5];
              else                    return 10 + log2_4[n >> 10];
    else if (n < (1 << 24))
