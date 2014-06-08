e2bbea3dee81acb0691be0f6ab9b2af40aeab7ed
Andrew Church <achurch@users.noreply.github.com>
2014-06-08 16:50:00 +0000

    Add a couple of tests for more distant seeks, using one of the stb_vorbis sample files.  The seek-to-new-page test seems to be broken at the moment.

---
+++ tests/seek/seek-to-new-frame.c
@@ -0,0 +1,41 @@
+/*
+ * libnogg: a decoder library for Ogg Vorbis streams
+ * Copyright (c) 2014 Andrew Church <achurch@achurch.org>
+ *
+ * This software may be copied and redistributed under certain conditions;
+ * see the file "COPYING" in the source code distribution for details.
+ * NO WARRANTY is provided with this software.
+ */
+
+#include "include/nogg.h"
+#include "include/test.h"
+
+
+int main(void)
+{
+    vorbis_t *vorbis;
+    EXPECT_TRUE(vorbis = vorbis_open_from_file("tests/data/thingy.ogg", NULL));
+
+    vorbis_seek(vorbis, 2944);
+
+    static const float expected_pcm[10] = {
+         0.05026849,
+         0.04001303,
+         0.02807940,
+         0.01561053,
+         0.00312608,
+        -0.01011315,
+        -0.02482782,
+        -0.04148402,
+        -0.06053595,
+        -0.08025991,
+    };
+    float pcm[10];
+    vorbis_error_t error = (vorbis_error_t)-1;
+    EXPECT_EQ(vorbis_read_float(vorbis, pcm, 10, &error), 10);
+    EXPECT_EQ(error, VORBIS_NO_ERROR);
+    COMPARE_PCM_FLOAT(pcm, expected_pcm, 10);
+
+    vorbis_close(vorbis);
+    return EXIT_SUCCESS;
+}
---
+++ tests/seek/seek-to-new-page.c
@@ -0,0 +1,42 @@
+/*
+ * libnogg: a decoder library for Ogg Vorbis streams
+ * Copyright (c) 2014 Andrew Church <achurch@achurch.org>
+ *
+ * This software may be copied and redistributed under certain conditions;
+ * see the file "COPYING" in the source code distribution for details.
+ * NO WARRANTY is provided with this software.
+ */
+
+#include "include/nogg.h"
+#include "include/test.h"
+
+
+int main(void)
+{
+    vorbis_t *vorbis;
+    EXPECT_TRUE(vorbis = vorbis_open_from_file("tests/data/thingy.ogg", NULL));
+
+    vorbis_seek(vorbis, 53632);
+
+    static const float expected_pcm[10] = {
+         0.29297784,
+         0.30478153,
+         0.31731880,
+         0.32574975,
+         0.32631603,
+         0.31716022,
+         0.29891869,
+         0.27464306,
+         0.24851273,
+         0.22406912,
+    };
+    float pcm[10];
+    vorbis_error_t error = (vorbis_error_t)-1;
+//FIXME: broken -- returns data from 2048 samples later
+    EXPECT_EQ(vorbis_read_float(vorbis, pcm, 10, &error), 10);
+    EXPECT_EQ(error, VORBIS_NO_ERROR);
+    COMPARE_PCM_FLOAT(pcm, expected_pcm, 10);
+
+    vorbis_close(vorbis);
+    return EXIT_SUCCESS;
+}
