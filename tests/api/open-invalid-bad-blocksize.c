ae44f0e18aad4526b2dd3a71ba5a7c5a546e0194
Andrew Church <achurch@users.noreply.github.com>
2014-06-10 12:29:14 +0900

    Get full test coverage for skip() in src/decode/io.c.

--- tests/api/open-invalid-bad-blocksize.c
+++ tests/api/open-invalid-bad-blocksize.c
@@ -11,13 +11,17 @@
 #include "include/test.h"
 
 
+static const char bad_blocksize_header[58] =
+    "OggS\0\2\0\0\0\0\0\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\1\x1E"
+    "\1vorbis\0\0\0\0\1\xA0\x0F\0\0\0\0\0\0\xFF\xFF\xFF\xFF"
+    "\0\0\0\0\x90\x01";
+
 int main(void)
 {
     vorbis_error_t error = (vorbis_error_t)-1;
     EXPECT_FALSE(vorbis_open_from_buffer(
-                     "OggS\0\2\0\0\0\0\0\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\1\x1E"
-                     "\1vorbis\0\0\0\0\1\xA0\x0F\0\0\0\0\0\0\xFF\xFF\xFF\xFF"
-                     "\0\0\0\0\x90\x01", 52, &error));
+                     bad_blocksize_header, sizeof(bad_blocksize_header),
+                     &error));
     EXPECT_EQ(error, VORBIS_ERROR_DECODE_SETUP_FAILURE);
 
     return EXIT_SUCCESS;
