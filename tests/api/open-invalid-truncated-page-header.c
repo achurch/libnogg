d67795616fc6ddc296d8c899e4875085f99fa8cb
Andrew Church <achurch@users.noreply.github.com>
2014-07-01 03:41:02 +0900

    Test handling of the VORBIS_unexpected_eof error during stream open.

---
+++ tests/api/open-invalid-truncated-page-header.c
@@ -0,0 +1,29 @@
+/*
+ * libnogg: a decoder library for Ogg Vorbis streams
+ * Copyright (c) 2014 Andrew Church <achurch@achurch.org>
+ *
+ * This software may be copied and redistributed under certain conditions;
+ * see the file "COPYING" in the source code distribution for details.
+ * NO WARRANTY is provided with this software.
+ */
+
+#include "include/nogg.h"
+#include "include/test.h"
+
+
+static const char truncated_page_header[85] =
+    "OggS\0\2\0\0\0\0\0\0\0\0\1\0\0\0\0\0\0\0\0\0\0\0\1\x1E"
+    "\1vorbis\0\0\0\0\1\xA0\x0F\0\0\0\0\0\0\xFF\xFF\xFF\xFF"
+    "\0\0\0\0\x99\x01"
+    "OggS\0\0\0\0\0\0\0\0\0\0\2\0\0\0\0\0\0\0\0\0\0\0\1";
+
+int main(void)
+{
+    vorbis_error_t error = (vorbis_error_t)-1;
+    EXPECT_FALSE(vorbis_open_from_buffer(
+                     truncated_page_header, sizeof(truncated_page_header),
+                     &error));
+    EXPECT_EQ(error, VORBIS_ERROR_STREAM_INVALID);
+
+    return EXIT_SUCCESS;
+}
