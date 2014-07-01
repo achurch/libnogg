6df61ae72432400cd6fa8bd060b881e8355c2d1d
Andrew Church <achurch@users.noreply.github.com>
2014-07-02 08:47:46 +0900

    Fix accidental loss of coverage in src/api.

--- tests/api/read-past-end.c
+++ tests/api/read-past-end.c
@@ -10,20 +10,35 @@
 #include "include/nogg.h"
 #include "include/test.h"
 
-#include "tests/data/square_float.h"  // Defines expected_pcm[].
+#define expected_pcm expected_pcm_float
+#include "tests/data/square_float.h"
+#undef expected_pcm
+
+#define expected_pcm expected_pcm_int16
+#include "tests/data/square_int16.h"
+#undef expected_pcm
 
 
 int main(void)
 {
     vorbis_t *vorbis;
+    float pcm_float[41];
+    int16_t pcm_int16[41];
+    vorbis_error_t error;
+
     EXPECT_TRUE(vorbis = vorbis_open_from_file("tests/data/square.ogg", NULL));
-
-    float pcm[41];
-    vorbis_error_t error = (vorbis_error_t)-1;
-    EXPECT_EQ(vorbis_read_float(vorbis, pcm, 41, &error), 40);
+    error = (vorbis_error_t)-1;
+    EXPECT_EQ(vorbis_read_float(vorbis, pcm_float, 41, &error), 40);
     EXPECT_EQ(error, VORBIS_ERROR_STREAM_END);
-    COMPARE_PCM_FLOAT(pcm, expected_pcm, 40);
-
+    COMPARE_PCM_FLOAT(pcm_float, expected_pcm_float, 40);
     vorbis_close(vorbis);
+
+    EXPECT_TRUE(vorbis = vorbis_open_from_file("tests/data/square.ogg", NULL));
+    error = (vorbis_error_t)-1;
+    EXPECT_EQ(vorbis_read_int16(vorbis, pcm_int16, 41, &error), 40);
+    EXPECT_EQ(error, VORBIS_ERROR_STREAM_END);
+    COMPARE_PCM_INT16(pcm_int16, expected_pcm_int16, 40);
+    vorbis_close(vorbis);
+
     return EXIT_SUCCESS;
 }
