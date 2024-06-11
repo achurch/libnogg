640ff32d11592cc819f6868c60d677184bca1168
Andrew Church <achurch@users.noreply.github.com>
2024-06-11 15:45:37 +0900

    Boilerplate update.

--- tools/gen-tables.pl
+++ tools/gen-tables.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 #
 # libnogg: a decoder library for Ogg Vorbis streams
-# Copyright (c) 2014-2023 Andrew Church <achurch@achurch.org>
+# Copyright (c) 2014-2024 Andrew Church <achurch@achurch.org>
 #
 # This software may be copied and redistributed under certain conditions;
 # see the file "COPYING" in the source code distribution for details.
